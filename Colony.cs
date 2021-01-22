4590e07135584d9f9a0ab7b71d4526b7a70a3e87
EsperControl <59237443+EsperControl@users.noreply.github.com>
2021-01-22 17:26:48 +0300

    Add files via upload

---
+++ Colony.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CelestialBody))]
+public class Colony : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //[SerializeField] тут должно быть объявление словаря тип ресурса/модификатор
+    [SerializeField] float pops = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
