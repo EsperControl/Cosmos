51e9c359af80406730b336726d3acbeb0102c75c
EsperControl <59237443+EsperControl@users.noreply.github.com>
2021-01-22 17:26:58 +0300

    Add files via upload

---
+++ Ship.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField] float cruiseSpeed = 1f;
+    [SerializeField] float rotationSpeed = 1f;
+    [SerializeField] float acceleration = 1f;
+    [SerializeField] float decceleration = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
